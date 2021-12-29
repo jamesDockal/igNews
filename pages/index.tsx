11dd93d5268c4d7fc3a9627806b432f65ad34711
James <jamesistranho@gmail.com>
2021-12-29 20:48:18 -0300

    chore: start project installing depedencies

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+
+import styles from "../styles/home.module.scss";
+
+const Home = ({}) => {
+  return <div className={styles.title}>teste</div>;
+};
+
+export default Home;
