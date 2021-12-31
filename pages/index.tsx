fb6b4f8344c0f3ade41a08a606f307e02e890cff
James <jamesistranho@gmail.com>
2021-12-30 22:03:58 -0300

    feat: create global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Head from "next/head";
-import Image from "next/image";
-
-import styles from "../styles/home.module.scss";
 
 const Home = ({}) => {
-  return <div className={styles.title}>teste</div>;
+  return (
+    <>
+      <Head>
+        <title>Inicio | Ig.News</title>
+      </Head>
+    </>
+  );
 };
 
 export default Home;
