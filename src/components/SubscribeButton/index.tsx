5d13fbbb6e5159fd62a715fe82b1d36d6080621e
James <jamesistranho@gmail.com>
2022-01-09 12:11:05 -0300

    feat: add redirect to payment of stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,8 +1,36 @@
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 export function SubscribeButton() {
+  const { data } = useSession();
+
+  async function handleSubscribe() {
+    if (!data) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      console.log("error.message", error.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
